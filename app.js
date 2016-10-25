a391325dd38785a446b9c0e9590f5f494b755035
Hou In Si Tou <hsitou@ucsd.edu>
2016-10-24 20:06:48 -0700

    Changed template engine, added coded up home page.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-var hbs = require('hbs');
 var express = require('express');
-var handlebars = require('handlebars');
-
+var routes = require('./routes/routes.js');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,8 +8,9 @@ app.use('/wireframe', express.static(__dirname + '/wireframe'));
 app.use(express.static(__dirname + '/includes'));
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
-app.engine('html', require('hbs').__express);
+app.set('view engine', 'ejs');
+
+app.use(routes);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
